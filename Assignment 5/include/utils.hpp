5bf056d5e60bbb41f0909d0167d47472c63fa65d
emilyberlinghoff <126015922+emilyberlinghoff@users.noreply.github.com>
2025-03-22 15:17:57 -0400

    Update utils.hpp
    moved std::vector<float> compute_normals(const std::vector<float>& vertices); from marching_cubes.hpp

--- Assignment 5/include/utils.hpp
+++ Assignment 5/include/utils.hpp
@@ -5,6 +5,11 @@
 #include <vector>
 #include <string>
 
+#pragma once
+#include <vector>
+
+std::vector<float> compute_normals(const std::vector<float>& vertices);
+
 std::string readShaderFile(const std::string &filePath);
 void checkShaderCompilation(GLuint shaderID);
 void checkProgramLinking(GLuint programID);

8591bf5c5238f43a99c68401affdf4f988bd53b5
emilyberlinghoff <126015922+emilyberlinghoff@users.noreply.github.com>
2025-03-22 15:16:49 -0400

    Update rendering.hpp

--- Assignment 5/include/rendering.hpp
+++ Assignment 5/include/rendering.hpp
@@ -1,12 +1,8 @@
-#ifndef RENDERING_HPP
-#define RENDERING_HPP
-
+#pragma once
 #include <GL/glew.h>
-#include <vector>
 #include <glm/glm.hpp>
+#include <vector>
 
-void setupRendering(GLuint &vao, GLuint &vertexVBO, GLuint &normalVBO, GLuint &programID);
 void updateBuffers(GLuint vao, GLuint vertexVBO, GLuint normalVBO, std::vector<float> vertices, std::vector<float> normals);
-void renderScene(GLuint vao, GLuint programID, glm::mat4 mvp, glm::mat4 view);
-
-#endif
+void renderScene(GLuint vao, GLuint programID, glm::mat4 mvp, glm::mat4 view, int vertexCount);
+void setupRendering(GLuint &vao, GLuint &vertexVBO, GLuint &normalVBO, GLuint programID);

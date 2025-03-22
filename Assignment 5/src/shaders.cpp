776f2b041c4fae17fe97ef1f7a532ab075ccb2fe
emilyberlinghoff <126015922+emilyberlinghoff@users.noreply.github.com>
2025-03-22 15:20:23 -0400

    Update shaders.cpp

--- Assignment 5/src/shaders.cpp
+++ Assignment 5/src/shaders.cpp
@@ -6,6 +6,10 @@
 
 std::string readShaderFile(const std::string &filePath) {
     std::ifstream file(filePath);
+    if (!file) {
+        std::cerr << "Failed to open shader file: " << filePath << std::endl;
+        return "";
+    }
     std::stringstream buffer;
     buffer << file.rdbuf();
     return buffer.str();
@@ -21,9 +25,24 @@ void checkShaderCompilation(GLuint shaderID) {
     }
 }
 
+void checkProgramLinkStatus(GLuint programID) {
+    GLint success;
+    glGetProgramiv(programID, GL_LINK_STATUS, &success);
+    if (!success) {
+        GLchar infoLog[512];
+        glGetProgramInfoLog(programID, 512, NULL, infoLog);
+        std::cerr << "Shader link error:\n" << infoLog << std::endl;
+    }
+}
+
 GLuint loadShaders(const char *vertex_file_path, const char *fragment_file_path) {
     std::string vertexCode = readShaderFile(vertex_file_path);
     std::string fragmentCode = readShaderFile(fragment_file_path);
+
+    if (vertexCode.empty() || fragmentCode.empty()) {
+        std::cerr << "Shader source code is empty. Aborting shader compilation." << std::endl;
+        return 0;
+    }
 
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     const char *vertexSource = vertexCode.c_str();
@@ -41,6 +60,7 @@ GLuint loadShaders(const char *vertex_file_path, const char *fragment_file_path)
     glAttachShader(programID, vertexShader);
     glAttachShader(programID, fragmentShader);
     glLinkProgram(programID);
+    checkProgramLinkStatus(programID);
 
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
